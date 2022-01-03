a65da944f76eea4d1d6716bde480ac2614be79da
btbrad <24836323+btbrad@users.noreply.github.com>
2022-01-03 15:15:43 +0800

    layout done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React from 'react'
-import { Switch, Route, Link } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 import { Layout, Typography, Space } from 'antd'
-import { Navbar } from './components'
+import { Navbar, Exchanges, Homepage, Cryptocurrencies, News, CryptoDetails } from './components'
 
 const App = () => {
   return (
-    <div>
+    <div className="app">
       <div className="navbar">
         <Navbar />
       </div>
-      <div className="main"></div>
-      <div className="footer"></div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path="/" element={<Homepage />} />
+              <Route exact path="/exchanges" element={<Exchanges />} />
+              <Route exact path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route exact path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route exact path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Typography.Title level={5} style={{color: '#fff', textAlign: 'center'}}>
+            Cryptoverse<br/>
+            All Rights Reserved.
+          </Typography.Title>
+          <Space></Space>
+        </div>
+      </div>
     </div>
   )
 }
