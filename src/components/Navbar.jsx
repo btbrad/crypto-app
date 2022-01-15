bb5958439e875578cf612f09be72bebf24392676
btbrad <24836323+btbrad@users.noreply.github.com>
2022-01-15 19:49:17 +0800

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Button, Menu, Typography, Avatar } from 'antd'
 import { Link } from 'react-router-dom'
 import {
@@ -6,11 +6,33 @@ import {
   MoneyCollectOutlined,
   BulbOutlined,
   FundOutlined,
-  MenusOutlined,
+  MenuOutlined,
 } from '@ant-design/icons'
 import Icon from '../images/logo192.png'
 
 const Navbar = () => {
+  const [btnVisible, setBtnVisible] = useState(false)
+  const [visible, setVisible] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener('load', handler)
+    window.addEventListener('resize', handler)
+    return () => {
+      window.removeEventListener('resize', handler)
+    }
+  }, [])
+
+  const handler = () => {
+    console.log(111)
+    if (document.body?.offsetWidth >= 800) {
+      setVisible(true)
+      setBtnVisible(false)
+    } else {
+      setVisible(false)
+      setBtnVisible(true)
+    }
+  }
+
   return (
     <div className="nav-container">
       <div className="logo-container">
@@ -18,21 +40,33 @@ const Navbar = () => {
         <Typography.Title level={2} className="logo">
           <Link to="/">Cryptoverse</Link>
         </Typography.Title>
+        {btnVisible && (
+          <MenuOutlined
+            style={{
+              color: '#fff',
+              fontSize: '25px',
+              cursor: 'pointer',
+            }}
+            onClick={() => setVisible(!visible)}
+          />
+        )}
       </div>
-      <Menu theme="dark">
-        <Menu.Item key="home" icon={<HomeOutlined />}>
-          <Link to="/">Home</Link>
-        </Menu.Item>
-        <Menu.Item key="cryptocurrencies" icon={<FundOutlined />}>
-          <Link to="/cryptocurrencies">Cryptocurrencies</Link>
-        </Menu.Item>
-        <Menu.Item key="exchanges" icon={<MoneyCollectOutlined />}>
-          <Link to="/exchanges">Exchanges</Link>
-        </Menu.Item>
-        <Menu.Item key="news" icon={<BulbOutlined />}>
-          <Link to="/news">News</Link>
-        </Menu.Item>
-      </Menu>
+      {visible && (
+        <Menu theme="dark">
+          <Menu.Item key="home" icon={<HomeOutlined />}>
+            <Link to="/">Home</Link>
+          </Menu.Item>
+          <Menu.Item key="cryptocurrencies" icon={<FundOutlined />}>
+            <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+          </Menu.Item>
+          <Menu.Item key="exchanges" icon={<MoneyCollectOutlined />}>
+            <Link to="/exchanges">Exchanges</Link>
+          </Menu.Item>
+          <Menu.Item key="news" icon={<BulbOutlined />}>
+            <Link to="/news">News</Link>
+          </Menu.Item>
+        </Menu>
+      )}
     </div>
   )
 }
